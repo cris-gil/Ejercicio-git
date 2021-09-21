d2dc8a493cc0cfff698c1d825519d773de06e152
Cristina Gil <cristinagil82@gmail.com>
2021-09-21 12:27:12 -0500

    ejercicio git

---
+++ ejercicio1.js
@@ -0,0 +1,12 @@
+/*Convertir a arrow function
+Empecemos por practicar la sintaxis de una arrow function que no recibe parámetros.
+
+Para este ejercicio vamos a dar tres funciones ya definidas. Nuestro trabajo será cambiarle la sintaxis y volver a escribirlas para que sean funciones de tipo arrow.
+Dame una pista
+Los conceptos para resolver el ejercicio se encuentran en los videos y material complementario.*/
+
+let dameCinco = () => [1, 2, 3, 4, 5];
+
+let multiplicarPorDos = () => 123 * 2;
+
+let mostrarNombre = () => "Mi nombre es Hernán";
---
+++ ejercicio2.js
@@ -0,0 +1,8 @@
+/*Arrow function con parámetro
+Ahora nuestro trabajo será refactorizar la función saludar() que recibe un parámetro, es decir, escribirla con la sintaxis necesaria para convertirla en una arrow function.
+Dame una pista
+No nos olvidemos los espacios entre las palabras en los strings. */
+
+let saludar = nombre => 'Hola, ' + nombre + '!';
+
+console.log(saludar(Cris));
